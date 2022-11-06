8dd8ff7ee6cfb738e4f91893b7daf68c83e440cc
Krystian <krystian.d.witek@gmail.com>
2022-11-06 14:04:31 +0100

    Create simple API

---
+++ src/main/kotlin/pl/witex/medicaresystemapi/controller/DoctorController.kt
@@ -0,0 +1,30 @@
+package pl.witex.medicaresystemapi.controller
+
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+@RequestMapping("/doctors")
+class DoctorController {
+
+    @GetMapping
+    fun getAll() = "all"
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: UUID) = "one"
+
+    @PostMapping
+    fun add() = "addOne"
+
+    @PutMapping
+    fun update() = "updateOne"
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: UUID) = "deleteOne"
+}
