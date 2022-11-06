01d807dad8b1dbaec5cbc68f098449eb88238b73
Krystian <krystian.d.witek@gmail.com>
2022-11-06 14:04:18 +0100

    Initial commit

---
+++ src/main/kotlin/pl/witex/medicaresystemapi/MedicareSystemApiApplication.kt
@@ -0,0 +1,11 @@
+package pl.witex.medicaresystemapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedicareSystemApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MedicareSystemApiApplication>(*args)
+}
---
+++ src/test/kotlin/pl/witex/medicaresystemapi/MedicareSystemApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.witex.medicaresystemapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedicareSystemApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
